6f1b4457b78ded246d679402f760b747719acc35
Alexey <alexdtat@gmail.com>
2024-07-31 17:20:12 +0300

    fix: license representation changes and module doc-strings addition.

---
+++ CPDShell/Core/algorithms/BayesianCPD/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module for Bayesian CPD algorithm's customization blocks.
+"""
+
+__author__ = "Alexey Tatyanenko"
+__copyright__ = "Copyright (c) 2024 Alexey Tatyanenko"
+__license__ = "SPDX-License-Identifier: MIT"
---
+++ CPDShell/Core/algorithms/BayesianCPD/abstracts/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module for abstract base classes for Bayesian CPD algorithm.
+"""
+
+__author__ = "Alexey Tatyanenko"
+__copyright__ = "Copyright (c) 2024 Alexey Tatyanenko"
+__license__ = "SPDX-License-Identifier: MIT"
