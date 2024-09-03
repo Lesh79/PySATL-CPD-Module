6c7de2033f039e13e8344417caa97b24b4a119d9
Lesh79 <Temerlan.axmetov.75@mail.ru>
2024-09-03 23:08:28 +0300

    Fix config file

--- CPDShell/main.py
+++ CPDShell/main.py
@@ -8,7 +8,7 @@ from CPDShell.shell import CPDShell
 with tempfile.TemporaryDirectory() as tempdir:
     saver = DatasetSaver(Path(), True)
     generated = ScipyDatasetGenerator().generate_datasets(
-        Path("tests/test_CPDShell/test_configs/test_config_exp.yml"), saver
+        Path("tests/test_CPDShell/test_configs/test_config_1.yml"), saver
     )
 
     cpd = CPDShell(generated["exp"][0])
