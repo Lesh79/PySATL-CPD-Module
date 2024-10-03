dd50742f6575aa799d8a8343b4ae372cddcfc36c
artemiipatov <patov.988@gmail.com>
2024-10-03 17:40:52 +0300

    move knn example to main.py

--- CPDShell/main.py
+++ CPDShell/main.py
@@ -6,6 +6,7 @@ from CPDShell.Core.algorithms.BayesianCPD.detectors.simple_detector import Simpl
 from CPDShell.Core.algorithms.BayesianCPD.hazards.constant_hazard import ConstantHazard
 from CPDShell.Core.algorithms.BayesianCPD.likelihoods.gaussian_likelihood import GaussianLikelihood
 from CPDShell.Core.algorithms.BayesianCPD.localizers.simple_localizer import SimpleLocalizer
+from CPDShell.Core.algorithms.knn_algorithm import KNNAlgorithm
 from CPDShell.generator.generator import ScipyDatasetGenerator
 from CPDShell.generator.saver import DatasetSaver
 from CPDShell.shell import CPDShell
@@ -17,17 +18,40 @@ saver = DatasetSaver(Path(), True)
 generated = ScipyDatasetGenerator().generate_datasets(Path(path_string), saver)
 data, expected_change_points = generated[distributions_name]
 
+print("Expected change points:", expected_change_points)
+
+# Graph algorithm demo
 graph_cpd = CPDShell(data)
 graph_cpd.scrubber.window_length = 150
 graph_cpd.scrubber.movement_k = 2.0 / 3.0
-
-print("Expected change points:", expected_change_points)
 
 res_graph = graph_cpd.run_cpd()
 res_graph.visualize(True)
 print("Graph algorithm")
 print(res_graph)
 
+
+# k-NN based algorithm demo
+def metric(obs1: float, obs2: float) -> float:
+    return abs(obs1 - obs2)
+
+
+K = 5
+THRESHOLD = 4.0
+
+knn_algorithm = KNNAlgorithm(metric, K, THRESHOLD)
+knn_cpd = CPDShell(data, knn_algorithm)
+
+knn_cpd.scrubber.window_length = 32
+knn_cpd.scrubber.movement_k = 0.5
+
+res_knn = knn_cpd.run_cpd()
+res_knn.visualize(True)
+print("k-NN based algorithm")
+print(res_knn)
+
+
+# Bayesian algorithm demo
 HAZARD_RATE = 200
 LEARNING_WINDOW_SIZE = 30
 THRESHOLD = 0.5
