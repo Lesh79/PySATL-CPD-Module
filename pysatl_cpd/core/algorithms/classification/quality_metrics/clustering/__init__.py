8531282c5f3af84545038bc1299f1d290a39d458
desiment <desiment@yandex.ru>
2026-03-17 03:10:40 +0300

    refactor: remove everything
    * remove benchmarking folder, it will be fully rewritten
    * remove examples, since interfaces is going to change
    * remove analysis module, it will be fully rewritten
    * remove generator module, it will be fully rewritten to support
      multivariate data and complex dataset cases
    * remove all algorithms, those will be reimplemented with new
      interfaces and correct tests
    * remove all tests: there nothing to test yet

--- pysatl_cpd/core/algorithms/classification/quality_metrics/clustering/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module for implementations of quality_metrics CPD algorithm clustering functions.
-"""
-
-__author__ = "Loikov Vladislav"
-__copyright__ = "Copyright (c) 2025 PySATL project"
-__license__ = "SPDX-License-Identifier: MIT"
