6178a97427d443a9604ae0187d654d38ca19b028
iraedeus <dtotjmyanin@mail.ru>
2026-04-09 04:40:06 +0300

    fix: ascii coding and copyrights

--- pysatl_cpd/analysis/metrics/classification/classification_metric.py
+++ pysatl_cpd/analysis/metrics/classification/classification_metric.py
@@ -1,3 +1,9 @@
+# -*- coding: ascii -*-
+
+__author__ = "Danil Totmyanin"
+__copyright__ = "Copyright (c) 2026 PySATL project"
+__license__ = "SPDX-License-Identifier: MIT"
+
 from abc import abstractmethod
 from collections.abc import Sequence
 from typing import Any
