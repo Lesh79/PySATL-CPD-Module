6087aea1b86f583ab018ea2b63e64c5b56594f2f
iraedeus <dtotjmyanin@mail.ru>
2026-04-09 22:56:09 +0300

    fix: add header to all files

--- tests/mocks/analysis/metrics/classification/simple.py
+++ tests/mocks/analysis/metrics/classification/simple.py
@@ -1,5 +1,9 @@
 # -*- coding: ascii -*-
 
+__author__ = "Danil Totmyanin"
+__copyright__ = "Copyright (c) 2026 PySATL project"
+__license__ = "SPDX-License-Identifier: MIT"
+
 from typing import Any
 
 from pysatl_cpd.analysis.labeled_data import LabeledData
