2d57baed22774a1575337e1fc7317e304e2ed425
Danil Totmyanin <144918156+iraedeus@users.noreply.github.com>
2026-04-11 00:47:30 +0300

    fix: ABC inheritance in RunMetric in analysis/metrics (#78)

--- pysatl_cpd/analysis/metrics/run_metric.py
+++ pysatl_cpd/analysis/metrics/run_metric.py
@@ -11,14 +11,14 @@ __author__ = "Danil Totmyanin"
 __copyright__ = "Copyright (c) 2026 PySATL project"
 __license__ = "SPDX-License-Identifier: MIT"
 
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from typing import Any
 
 from pysatl_cpd.analysis.labeled_data import LabeledData
 from pysatl_cpd.core.detection_trace import DetectionTrace
 
 
-class RunMetric[TraceT: DetectionTrace, ProviderT: LabeledData[Any], ResultT]:
+class RunMetric[TraceT: DetectionTrace, ProviderT: LabeledData[Any], ResultT](ABC):
     """
     Base class for all run evaluation metrics in change point detection.
 
